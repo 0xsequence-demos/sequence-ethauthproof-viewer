ed230924bfef7c23cccbdcf033cb9806151e542f
Tomasz Dysinski <dysinski.tomasz@gmail.com>
2025-05-28 08:59:22 -0700

    updating tool

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-import '@0xsequence/design-system/styles.css'
-import {ThemeProvider} from '@0xsequence/design-system'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { ThemeProvider } from "@0xsequence/design-system";
+import { Toaster } from "sonner";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ThemeProvider>
-    <App />
+      <App />
+      <Toaster />
     </ThemeProvider>
   </React.StrictMode>,
-)
+);
